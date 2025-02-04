7710312ae47f3fde18b9d6fe37a50db2e369c8a6
Pratik Kumar <141059526+pratikkr75@users.noreply.github.com>
2025-02-04 14:44:43 +0530

    Update ClaimForm.jsx with render backend url

--- frontend/frontend/src/components/Claims/ClaimForm.jsx
+++ frontend/frontend/src/components/Claims/ClaimForm.jsx
@@ -92,7 +92,7 @@ const ClaimForm = ({ onSubmitSuccess }) => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/claims', {
+      const response = await fetch('https://csm-stateful-backend.onrender.com/api/claims', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
